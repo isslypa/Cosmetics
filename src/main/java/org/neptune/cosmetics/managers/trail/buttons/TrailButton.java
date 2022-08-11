aeb53145fcb09f0221fa0944517e4f899fa45ae6
isslypa <juan2207gomez@gmail.com>
2022-08-11 11:27:54 -0500

    read TrailButton.java

--- src/main/java/org/neptune/cosmetics/managers/trail/buttons/TrailButton.java
+++ src/main/java/org/neptune/cosmetics/managers/trail/buttons/TrailButton.java
@@ -1,14 +1,17 @@
 package org.neptune.cosmetics.managers.trail.buttons;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.neptune.cosmetics.Cosmetics;
 import org.neptune.cosmetics.managers.data.PlayerData;
+import org.neptune.cosmetics.utilities.file.FileHandler;
 import org.neptune.cosmetics.utilities.item.ItemCreator;
 
 public class TrailButton {
+   private static final FileHandler menu = Cosmetics.getInstance().getMenu();
    public static ItemStack getRemove(Player player) {
       PlayerData data = new PlayerData(player.getUniqueId());
-      return (new ItemCreator(Material.RECORD_4)).setName("&c&lRemove Trail").setLore("", "&cTrail&7: &r" + (data.getTrail() == null ? "None" : data.getTrail()), "", "&7Click to remove your currently trail.").get();
-   }
+         return (new ItemCreator(menu.getString("COSMETICS.TRAIL.BUTTON.ITEM.MATERIAL")).setName(menu.getString("COSMETICS.TRAIL.BUTTON.NAME")).setLore(menu.getStringList("COSMETICS.TRAIL.BUTTON.LORE") + (data.getTrail() == null ? "None" : data.getTrail())).get());
+         //Hay que hacer una variable para (data.getTrail() == null ? "None" : data.getTrail()) llamada %ACTIVE_TRAILS%". esto es para el lore.
+      }
 }
